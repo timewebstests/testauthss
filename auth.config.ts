a231f52257995fc796dc8826ee39aee72f7c07c3
Vulnet Sejfulai <timewebsorder@gmail.com>
2024-03-16 01:21:36 +0100

    ok commit

--- auth.config.ts
+++ auth.config.ts
@@ -3,6 +3,7 @@ import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google"
 import Github from "next-auth/providers/Github"
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
